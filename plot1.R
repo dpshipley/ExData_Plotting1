08a318d50f67ded3c414be65b4fc3e694974931f
dpshipley <dpshipley@micron.com>
2015-03-05 12:11:40 -0800

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -4,5 +4,6 @@ dt <- read.table ("household_power_consumption.txt",header = TRUE, sep = ";",str
 #Create subset based on date range
 dt<- subset(dt, (dt$Date == "1/2/2007" | dt$Date== "2/2/2007"))
 #Build plot and create png
-hist(dt$Global_active_power, col="red", main = "Global Active Power", xlab= "Global Active Power (kilowatts)", ylab= "Frequency")
 png("plot1.png", width=480, height= 480)
+hist(dt$Global_active_power, col="red", main = "Global Active Power", xlab= "Global Active Power (kilowatts)", ylab= "Frequency")
+dev.off()
